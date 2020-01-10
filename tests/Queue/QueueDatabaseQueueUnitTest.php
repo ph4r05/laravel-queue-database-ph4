b427bf29c72bba3489787d92a1852c9d86b23d9b
Jinx <matei.georgian04@gmail.com>
2020-01-10 23:28:09 +0200

    updated style

--- tests/Queue/QueueDatabaseQueueUnitTest.php
+++ tests/Queue/QueueDatabaseQueueUnitTest.php
@@ -26,7 +26,7 @@ class QueueDatabaseQueueUnitTest extends TestCase
         $database->shouldReceive('table')->with('table')->andReturn($query = m::mock('stdClass'));
         $query->shouldReceive('insertGetId')->once()->andReturnUsing(function ($array) {
             $this->assertEquals('default', $array['queue']);
-            $this->assertEquals(json_encode(['displayName' => 'foo', 'job' => 'foo', 'maxTries' => null,'delay'=>null, 'timeout' => null, 'data' => ['data']]), $array['payload']);
+            $this->assertEquals(json_encode(['displayName' => 'foo', 'job' => 'foo', 'maxTries' => null, 'delay'=>null, 'timeout' => null, 'data' => ['data']]), $array['payload']);
             $this->assertEquals(0, $array['attempts']);
             $this->assertNull($array['reserved_at']);
             $this->assertInternalType('int', $array['available_at']);
@@ -46,7 +46,7 @@ class QueueDatabaseQueueUnitTest extends TestCase
         $database->shouldReceive('table')->with('table')->andReturn($query = m::mock('stdClass'));
         $query->shouldReceive('insertGetId')->once()->andReturnUsing(function ($array) {
             $this->assertEquals('default', $array['queue']);
-            $this->assertEquals(json_encode(['displayName' => 'foo', 'job' => 'foo', 'maxTries' => null,'delay'=>null, 'timeout' => null, 'data' => ['data']]), $array['payload']);
+            $this->assertEquals(json_encode(['displayName' => 'foo', 'job' => 'foo', 'maxTries' => null, 'delay'=>null, 'timeout' => null, 'data' => ['data']]), $array['payload']);
             $this->assertEquals(0, $array['attempts']);
             $this->assertNull($array['reserved_at']);
             $this->assertInternalType('int', $array['available_at']);
@@ -102,7 +102,7 @@ class QueueDatabaseQueueUnitTest extends TestCase
         $query->shouldReceive('insert')->once()->andReturnUsing(function ($records) {
             $this->assertEquals([[
                 'queue'        => 'queue',
-                'payload'      => json_encode(['displayName' => 'foo', 'job' => 'foo', 'maxTries' => null,'delay'=>null, 'timeout' => null, 'data' => ['data']]),
+                'payload'      => json_encode(['displayName' => 'foo', 'job' => 'foo', 'maxTries' => null, 'delay'=>null, 'timeout' => null, 'data' => ['data']]),
                 'attempts'     => 0,
                 'reserved_at'  => null,
                 'available_at' => 'available',
@@ -110,7 +110,7 @@ class QueueDatabaseQueueUnitTest extends TestCase
                 'version'      => 0,
             ], [
                 'queue'        => 'queue',
-                'payload'      => json_encode(['displayName' => 'bar', 'job' => 'bar', 'maxTries' => null,'delay'=>null, 'timeout' => null, 'data' => ['data']]),
+                'payload'      => json_encode(['displayName' => 'bar', 'job' => 'bar', 'maxTries' => null, 'delay'=>null, 'timeout' => null, 'data' => ['data']]),
                 'attempts'     => 0,
                 'reserved_at'  => null,
                 'available_at' => 'available',
